832e7cd9fa36ee8cda708da0bb2ebd88cbb6da3e
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-05-12 18:06:40 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,34 @@
+class Owner
+  
+  @@all = []
+  def initialize(name) 
+    @name = name 
+    @species = 'human'
+    @@all << self
+  end
+  
+  def name 
+    @name 
+  end 
+  
+  def species 
+    @species 
+  end 
+  
+  def say_species 
+    "I am a #{@species}."
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
   def self.count
     @@all.length
   end 
+  
+  def self.reset_all
+    @@all = []
+  end 
+  
+end
